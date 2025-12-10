ed1e281a70539f5b26f3a47005d0d1132654075c
tribxieee <denaneer11@gmail.com>
2025-12-10 20:13:50 +0700

    update task list

--- js/script.js
+++ js/script.js
@@ -7,3 +7,18 @@ document.querySelector(".btn-add").addEventListener("click", (e) => {
         draggable: true
     });
 });
+
+document.querySelector(".clear-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    Swal.fire({
+    title: "Do you want to delete?",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    denyButtonText: `No`
+    }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire("Success Delete Task!", "", "success");
+    }
+    });
+});
